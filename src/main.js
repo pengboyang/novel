9f2dd60ef283c9bf588d3aca145c53f4418541fc
Y <pengbo_zjj@163.com>
2018-09-20 21:18:24 +0800

    share

--- src/main.js
+++ src/main.js
@@ -10,10 +10,9 @@ import 'swiper/dist/css/swiper.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import WeVue from 'we-vue'
 import 'we-vue/lib/style.css'
-import wx from 'weixin-jsapi'
 import 'mint-ui/lib/style.css';
 import MintUI from 'mint-ui'
-
+import wxConfig from '@/js/wxconfig.js';
 
 var VueTouch = require('vue-touch')
 Vue.use(VueTouch, {name: 'v-touch'})
@@ -108,38 +107,39 @@ router.afterEach((to, from) => {
   if (to.meta.title) {
     document.title = to.meta.title;
   }
-  Vue.$http({
-    method: 'get',
-    url: '/novel/wechat/configSign',
-    params: {
-      url:location.href.split('#')[0]
-    }
-  }).then(res => {
-    console.log(res);
-    var data = res.data;
-    if (data.status == 1) {
-      console.log('config start');
-      wx.config({
-        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        appId: 'wx30e74a0a5ca3c0bd', // 必填，公众号的唯一标识
-        timestamp: data.timestamp, // 必填，生成签名的时间戳
-        nonceStr: data.nonceStr, // 必填，生成签名的随机串
-        signature: data.signature,// 必填，签名
-        jsApiList: [
-          'chooseWXPay'
-        ] // 必填，需要使用的JS接口列表
-      });
-      wx.ready(function(){
-        console.log('wxconfig success');
-      });
-      wx.error(function(res){
-        console.log(res);
-        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-      });
-    }
-  }).catch(error => {
-    console.log(error);
-  });
+  wxConfig();
+  // Vue.$http({
+  //   method: 'get',
+  //   url: '/novel/wechat/configSign',
+  //   params: {
+  //     url:location.href.split('#')[0]
+  //   }
+  // }).then(res => {
+  //   console.log(res);
+  //   var data = res.data;
+  //   if (data.status == 1) {
+  //     console.log('config start');
+  //     wx.config({
+  //       debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+  //       appId: 'wx30e74a0a5ca3c0bd', // 必填，公众号的唯一标识
+  //       timestamp: data.timestamp, // 必填，生成签名的时间戳
+  //       nonceStr: data.nonceStr, // 必填，生成签名的随机串
+  //       signature: data.signature,// 必填，签名
+  //       jsApiList: [
+  //         'chooseWXPay'
+  //       ] // 必填，需要使用的JS接口列表
+  //     });
+  //     wx.ready(function(){
+  //       console.log('wxconfig success');
+  //     });
+  //     wx.error(function(res){
+  //       console.log(res);
+  //       // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+  //     });
+  //   }
+  // }).catch(error => {
+  //   console.log(error);
+  // });
 });
 
 /* eslint-disable no-new */
